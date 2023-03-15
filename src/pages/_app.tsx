370eed0e309dec9b422b9bf06067e1faa6f88e05
Zack Xu <ziangxuu@gmail.com>
2023-03-15 11:58:15 -0400

    restore demo.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import '@/styles/globals.css'
 import '@/styles/bootstrap-grid.min.css'
 import '@/styles/style.css'
 import '@/styles/responsive.css'
-// import '@/styles/demo.css'
+import '@/styles/demo.css'
 // import '@/styles/settings.css'
 // import '@/styles/settings-ver.5.2.3.css'
 
